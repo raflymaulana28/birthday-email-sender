01abbf010899a08364ce41f8bbd56db488335020
raflymaulana28 <raflymaulana.works@gmail.com>
2023-11-08 12:56:46 +0700

    :rocket: update create connection

--- dist/index.js
+++ dist/index.js
@@ -23,7 +23,15 @@ node_cron_1.default.schedule("* * * * * *", async () => {
 });
 async function initializeDatabase() {
     try {
-        await (0, typeorm_1.createConnection)();
+        await (0, typeorm_1.createConnection)({
+            name: "default",
+            type: "mongodb",
+            url: "mongodb://murahYohAyok55:persib123@cluster0-shard-00-00.2vhgo.mongodb.net:27017,cluster0-shard-00-01.2vhgo.mongodb.net:27017,cluster0-shard-00-02.2vhgo.mongodb.net:27017/?ssl=true&replicaSet=atlas-zowunz-shard-0&authSource=admin&retryWrites=true&w=majority",
+            entities: [User_1.User],
+            synchronize: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Connected to the database");
         app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
